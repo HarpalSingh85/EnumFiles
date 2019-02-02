01f75b53d526665b595b0bc92d1f076dbff8eb38
HarpalSingh85 <harpalinsatible@gmail.com>
2019-02-02 10:00:28 +0530

    Add files via upload

---
+++ EnumFiles/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\asinghh8";
+            int count = WinAPIEnum.FindFilesAndDirs(path).Count();
+            Console.WriteLine(count);
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
